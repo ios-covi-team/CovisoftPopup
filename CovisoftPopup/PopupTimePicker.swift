12a7ce218a1a07ea2128fafa88ee649a957642d3
Kai Luu <khang.luu@covisoft.asia>
2019-05-15 18:12:31 +0700

    Lastest code

--- CovisoftPopup/PopupTimePicker.swift
+++ CovisoftPopup/PopupTimePicker.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+public typealias ActionCallback = (_ time:String)->()
 
 public class PopupTimePicker: UIView {
     
@@ -22,11 +23,15 @@ public class PopupTimePicker: UIView {
     @IBOutlet weak var btnRight: UIButton!
     @IBOutlet weak var btnLeft: UIButton!
     
-    public let datePicker = UIDatePicker()
-    public let toolbar = UIToolbar()
-    public var dateFormatter = DateFormatter()
+     let datePicker = UIDatePicker()
+     let toolbar = UIToolbar()
+     var dateFormatter = DateFormatter()
     public var code:String?
     public var time:String?
+    
+    var isSelectedTime = false
+    
+    var actionCallback:ActionCallback?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -38,61 +43,64 @@ public class PopupTimePicker: UIView {
         loadViewFromNib ()
     }
     
-    public class func showPopupSetTime(code:String,time:String,callback:@escaping (_ isOk:Bool,_ time:String?,_ code:String?) -> ()) {
+    public class func showPopupSetTime(code:String,time:String,titleButtonLeft:String,titleButtonRight:String,callback:@escaping (_ isOk:Bool,_ time:String?,_ code:String?,_ isLeft:Bool?) -> ()) {
         let topVC = UIApplication.topViewController()
-        if let popTimePicker:PopupTimePicker = PopupTimePicker(frame: (topVC?.view.bounds)!) {
+        
+        let popTimePicker:PopupTimePicker = PopupTimePicker(frame: (topVC?.view.bounds)!)
+        if topVC?.view.window != nil {
+            topVC?.view.window!.addSubview(popTimePicker)
+        } else {
             topVC?.view.addSubview(popTimePicker)
+        }
+        
             
             popTimePicker.code = code
             popTimePicker.time = time
             popTimePicker.tfContent.text = time
-            popTimePicker.tfContent.delegate = popTimePicker
             popTimePicker.lblContent.text = code
-            
+        
+            popTimePicker.btnLeft.setTitle(titleButtonLeft, for: .normal)
+            popTimePicker.btnRight.setTitle(titleButtonRight, for: .normal)
+
             popTimePicker.btnClose.add(for: .touchUpInside) {
+                
                 popTimePicker.animationDisapprea(callback: { (finish) in
-                    callback(false,nil,nil)
+                    callback(false,nil,nil, nil)
                 })
             }
             
             popTimePicker.btnLeft.add(for: .touchUpInside) {
+                popTimePicker.textFieldDidEndEditing(popTimePicker.tfContent)
                 popTimePicker.animationDisapprea(callback: { (finish) in
-                    callback(false,nil,nil)
+                    if finish {
+                        callback(true,popTimePicker.isSelectedTime == true ? popTimePicker.time : time,code, true)
+                    }
                 })
-                
             }
             
             popTimePicker.btnRight.add(for: .touchUpInside) {
+                popTimePicker.textFieldDidEndEditing(popTimePicker.tfContent)
+
                 popTimePicker.animationDisapprea(callback: { (finish) in
-                    callback(true,popTimePicker.tfContent.text,code)
+                    if finish {
+                        callback(true,popTimePicker.isSelectedTime == true ? popTimePicker.time : time,code, false)
+                    }
                 })
-                
             }
-        }
-     
-        
     }
-    
-    
-    
     
     func loadViewFromNib() {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: "PopupTimePicker", bundle: bundle)
-        
         vContent = nib.instantiate(withOwner: self, options: nil).first as? UIView
         vContent?.frame = bounds
         vContent?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.addSubview(vContent!)
-        
+        tfContent.delegate = self
         animationApprea()
-        
         showDatePicker(txtDatePicker: tfContent)
-       
-
     }
     
- 
     
     func animationDisapprea(callback:@escaping(_ isFinish:Bool) ->()) {
         self.vPopup.transform = CGAffineTransform.init(scaleX: 1.0, y: 1.0)
@@ -110,10 +118,8 @@ public class PopupTimePicker: UIView {
                         callback(finished)
                     }
                 }
-                
             }
         }
-        
     }
     
     func animationApprea() {
@@ -136,7 +142,6 @@ public class PopupTimePicker: UIView {
         datePicker.locale = Locale(identifier: "da_DK")
         var date:Date?
         date = Date().getDateFromString(DateStr: tfContent.text, format: Date.dateFormat11)!
-        
         if date != nil  {
             datePicker.date = date!
         }
@@ -145,9 +150,8 @@ public class PopupTimePicker: UIView {
         toolbar.sizeToFit()
         let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(donedatePicker));
         let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
-        
-        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+       
+        toolbar.setItems([spaceButton,doneButton], animated: false)
         
         txtDatePicker.inputAccessoryView = toolbar
         txtDatePicker.inputView = datePicker
@@ -155,20 +159,24 @@ public class PopupTimePicker: UIView {
     }
     
     @objc func donedatePicker() {
-        self.endEditing(true)
+        isSelectedTime = true
+        textFieldDidEndEditing(tfContent)
+        tfContent.text = time
+
     }
     
     @objc func cancelDatePicker() {
-        datePicker.removeFromSuperview()
         self.endEditing(true)
     }
     
 }
 
 extension PopupTimePicker: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
+    private func textFieldDidEndEditing(_ textField: UITextField) {
         let dateString = datePicker.date.toString()
         let date = Date().getDateFromString(DateStr: dateString, format: Date.dateFormat4)
-        tfContent.text = Date().toString(date: date!, format: Date.dateFormatHourMin)
+        
+        time =  Date().toString(date: date!, format: Date.dateFormatHourMin)
+        textField.resignFirstResponder()
     }
 }
